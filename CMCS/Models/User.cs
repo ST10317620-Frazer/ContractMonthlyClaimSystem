a4751b7a19ef4c9ce11056d0a4382897e54a34e1
ST10317620-Frazer <129761537+ST10317620-Frazer@users.noreply.github.com>
2025-10-22 23:56:00 +0200

    another left out comment

--- CMCS/Models/User.cs
+++ CMCS/Models/User.cs
@@ -6,6 +6,6 @@ namespace CMCS.Models
         public required string Username { get; set; }
         public required string PasswordHash { get; set; } //password funtionality
         public required string Role { get; set; }//establish roles to implement role based access
-        public DateTime? DateProcessed { get; set; }
+        public DateTime? DateProcessed { get; set; }// time that the admin will either approve or reject claim
     }
 }
