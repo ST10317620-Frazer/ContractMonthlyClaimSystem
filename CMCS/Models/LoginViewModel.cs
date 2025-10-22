5ca36b61e32aab3c92dbab1f4585bf9d9bd5f862
ST10317620-Frazer <129761537+ST10317620-Frazer@users.noreply.github.com>
2025-10-22 23:54:32 +0200

    left out a comment

--- CMCS/Models/LoginViewModel.cs
+++ CMCS/Models/LoginViewModel.cs
@@ -1,6 +1,6 @@
 namespace CMCS.Models
 {
-    public class LoginViewModel
+    public class LoginViewModel //view for login
     {
         public required string Username { get; set; }
         public required string Password { get; set; }
